84ec0cdc3a2f0abe523283974b5b2d78632ed16d
Saransh <imsaranshy@gmail.com>
2025-03-16 04:13:34 +0530

    BackendUrl Modified Again!

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://course-selling-app-hg3i.onrender.com";
+export const BACKEND_URL = "https://course-selling-app-hg3i.onrender.com/api/v1";
 // export const BACKEND_URL = "http://localhost:4001/api/v1";
